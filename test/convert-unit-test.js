3324e63d5f69748503f9114a253c8f8bd5ba253d
Tim Robinson <tim@timjrobinson.com>
2014-03-14 16:20:45 +0000

    First commit, doesn't do anything yet

---
+++ test/convert-unit-test.js
@@ -0,0 +1,18 @@
+
+describe("side2nw", function() {
+    describe("run", function() {
+
+    });
+    describe("processFile", function() {
+
+    });
+    describe("getNewPath", function() {
+
+    });
+    describe("parse", function() {
+
+    });
+    describe("convert", function() {
+
+    });
+});
