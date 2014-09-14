8314b95b2d4b8d7f35e38c2a25920f140cadd208
Tim Robinson <tim@timjrobinson.com>
2014-09-14 16:39:18 +1000

    Removing extronous useCss and useXPath's everywhere

--- test/side2nw-unit-test.js
+++ test/side2nw-unit-test.js
@@ -1,3 +1,5 @@
+var assert = require('assert');
+var side2nw = require('../lib/side2nw.js');
 
 describe("side2nw", function() {
     describe("run", function() {
@@ -18,4 +20,18 @@ describe("side2nw", function() {
     describe("createCommands", function() {
 
     });
+    describe("sanitizeCommands", function() {
+        it("Should remove extra useCss's", function() {
+            var commands = ["useCss()", "test", "useCss()", "anotherTest"];
+            var sanitizedCommands = side2nw.sanitizeCommands(commands);
+            assert.deepEqual(sanitizedCommands, ["useCss()", "test", "anotherTest"]);
+        });
+
+        it("Should remove extra useXPath's", function() {
+            var commands = ["useXpath()", "test", "useXpath()", "anotherTest"];
+            var sanitizedCommands = side2nw.sanitizeCommands(commands);
+            assert.deepEqual(sanitizedCommands, ["useXpath()", "test", "anotherTest"]);
+        });
+    })
+
 });
